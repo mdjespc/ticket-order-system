ca4757b47b2b24c6ab4778c4545ba7cab2b02a66
mdjespc <mdjespc@gmail.com>
2025-01-29 21:20:54 -0500

    Initial queue model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import model.Node;
+import model.Queue;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ticket Order System");
+
+        Queue<Node<Integer>> queue = new Queue<>();
+
+        queue.enqueue(new Node<>(10));
+        queue.enqueue(new Node<>(20));
+        queue.enqueue(new Node<>(30));
+        queue.enqueue(new Node<>(40));
+        queue.enqueue(new Node<>(50));
+
+        System.out.println(queue.at(0));
+        System.out.println(queue.at(1));
+        System.out.println(queue.at(2));
+        System.out.println(queue.at(3));
+        System.out.println(queue.at(4));
     }
 }
--- src/model/Queue.java
+++ src/model/Queue.java
@@ -1,9 +1,47 @@
 package model;
 
 public class Queue <T> {
-    private T[] elements;
+    private Object[] elements;
 
+    private int maxSize;
     private int size;
     private int length;
-    
+
+    public Queue(){
+        this.size = 1;
+        this.length = 0;
+        this.elements = new Object[size];
+    }
+
+    public void enqueue(T value){
+        this.elements[length++] = value;
+
+        if (length == size)
+            reallocateElements(size + 1);
+
+    }
+
+    public T at(int index){
+        return (T) this.elements[index];
+    }
+
+    private void reallocateElements(int size){
+        if (size <= this.size) return;
+
+        this.elements = copyOfArray(size);
+        this.size = this.elements.length;
+    }
+
+    private Object[] copyOfArray(int size){
+        if (size <= this.size) return null;
+
+        var copy = new Object[size];
+
+        for (int i = 0; i < elements.length; i++){
+            copy[i] = elements[i];
+        }
+
+        return copy;
+    }
+
 }
